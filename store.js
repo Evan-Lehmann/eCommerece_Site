77545af76e0447083bd2956a42583bba0ad8a366
Evan-Lehmann <67935505+Evan-Lehmann@users.noreply.github.com>
2021-03-28 13:56:14 -0500

    Create store.js

---
+++ store.js
@@ -0,0 +1,219 @@
+
+    x.innerHTML = "Product #2"
+    x.style.display = "block";
+    x.style.fontSize = "1.2em"
+
+
+}
+
+function myFunc3() {
+    let x = document.getElementById("items3")
+    let y = document.getElementById("del3")
+    let z = document.getElementById("hiddenimg3")
+    z.style.display ="block"
+
+    y.style.display = "block";
+
+
+    x.innerHTML = "Product #3"
+    x.style.display = "block";
+    x.style.fontSize = "1.2em"
+
+
+}
+
+function myFunc4() {
+    let x = document.getElementById("items4")
+    let y = document.getElementById("del4")
+    let z = document.getElementById("hiddenimg4")
+    z.style.display ="block"
+
+    y.style.display = "block";
+
+
+    x.innerHTML = "Product #4"
+    x.style.display = "block";
+    x.style.fontSize = "1.2em"
+
+
+}
+
+function myFunc5() {
+    let x = document.getElementById("items5")
+    let y = document.getElementById("del5")
+    let z = document.getElementById("hiddenimg5")
+    z.style.display ="block"
+
+
+    y.style.display = "block";
+
+
+    x.innerHTML = "Product #5"
+    x.style.display = "block";
+    x.style.fontSize = "1.2em"
+
+
+}
+
+function myFunc6() {
+    let x = document.getElementById("items6")
+    let y = document.getElementById("del6")
+    let z = document.getElementById("hiddenimg6")
+    z.style.display ="block"
+
+    y.style.display = "block";
+    x.style.fontSize = "1.2em"
+
+
+
+    x.innerHTML = "Product #6"
+    x.style.display = "block";
+
+}
+
+function myFunc7() {
+    let x = document.getElementById("items7")
+    let y = document.getElementById("del7")
+    let z = document.getElementById("hiddenimg7")
+    z.style.display ="block"
+
+    y.style.display = "block";
+
+
+    x.innerHTML = "Product #7"
+    x.style.display = "block";
+    x.style.fontSize = "1.2em"
+
+
+}
+
+function myFunc8() {
+    let x = document.getElementById("items8")
+    let y = document.getElementById("del8")
+    let z = document.getElementById("hiddenimg8")
+    z.style.display ="block"
+
+    y.style.display = "block";
+
+
+    x.innerHTML = "Product #8"
+    x.style.display = "block";
+    x.style.fontSize = "1.2em"
+
+
+}
+
+function myFunc9() {
+    let x = document.getElementById("items9")
+    let y = document.getElementById("del9")
+    let z = document.getElementById("hiddenimg9")
+    z.style.display ="block"
+
+    y.style.display = "block";
+
+    x.style.fontSize = "1.2em"
+
+
+    x.innerHTML = "Product #9"
+    x.style.display = "block";
+
+}
+
+function del1() {
+    let x = document.getElementById("items1")
+    let y = document.getElementById("del1")
+    let z = document.getElementById("hiddenimg")
+    z.style.display ="none"
+    
+    y.style.display = "none";
+    x.style.display = "none";
+
+}
+
+function del2() {
+    let x = document.getElementById("items2")
+    let y = document.getElementById("del2")
+    let z = document.getElementById("hiddenimg2")
+    z.style.display ="none"
+    
+    y.style.display = "none";
+    x.style.display = "none";
+
+}
+
+function del3() {
+    let x = document.getElementById("items3")
+    let y = document.getElementById("del3")
+    let z = document.getElementById("hiddenimg3")
+    z.style.display ="none"
+    
+    y.style.display = "none";
+    x.style.display = "none";
+
+}
+
+function del4() {
+    let x = document.getElementById("items4")
+    let y = document.getElementById("del4")
+    let z = document.getElementById("hiddenimg4")
+    z.style.display ="none"
+    
+    y.style.display = "none";
+    x.style.display = "none";
+
+}
+
+function del5() {
+    let x = document.getElementById("items5")
+    let y = document.getElementById("del5")
+    let z = document.getElementById("hiddenimg5")
+    z.style.display ="none"
+    
+    y.style.display = "none";
+    x.style.display = "none";
+
+}
+
+function del6() {
+    let x = document.getElementById("items6")
+    let y = document.getElementById("del6")
+    let z = document.getElementById("hiddenimg6")
+    z.style.display ="none"
+    
+    y.style.display = "none";
+    x.style.display = "none";
+
+}
+
+function del7() {
+    let x = document.getElementById("items7")
+    let y = document.getElementById("del7")
+    let z = document.getElementById("hiddenimg7")
+    z.style.display ="none"
+    
+    y.style.display = "none";
+    x.style.display = "none";
+
+}
+
+function del8() {
+    let x = document.getElementById("items8")
+    let y = document.getElementById("del8")
+    let z = document.getElementById("hiddenimg8")
+    z.style.display ="none"
+    
+    y.style.display = "none";
+    x.style.display = "none";
+
+}
+
+function del9() {
+    let x = document.getElementById("items9")
+    let y = document.getElementById("del9")
+    let z = document.getElementById("hiddenimg9")
+    z.style.display ="none"
+    
+    y.style.display = "none";
+    x.style.display = "none";
+}
+
